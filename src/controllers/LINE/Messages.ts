743d89a9597934cb752a1e9b80003ae99851e9b6
ardikabs <ardikabs@gmail.com>
2017-11-16 22:43:02 +0700

    add

--- src/controllers/LINE/Messages.ts
+++ src/controllers/LINE/Messages.ts
@@ -149,38 +149,39 @@ class Messages{
                     continue;
                 }
 
+                mapsClient.placesPhoto(photoQuery, (err,res)=>{
+                    if(err)
+                        console.log("Error query place photo : ", err);
+                    
+                    carouselMsg.addColumn(
+                        "https://" + res.req.socket._host + "" + res.req.path,
+                        trimString40(result[i].name),
+                        trimString60(result[i].vicinity),
+                        [
+                            ActionBuilder.createUriAction("Liat Map","https://www.google.com/maps/@"+result[i].geometry.location.lat+","+result[i].geometry.location.lng+",20z")
+                        ]
+                    );
+                    
+                    if(carouselMsg.column.length === limit){
+                        msg = carouselMsg.build();
+                        this.event.reply(msg);     
+                    }
+                    else if(carouselMsg.column.length === 0 && i === (resultLength-1)){
+                        console.log("Ngga ketemu apa apa");                      
+                        msg = 'Aku ngga bisa nemuin tempat makan dengan radius 1KM dari tempat kamu nih, coba jalan aja dulu';
+                        this.event.reply(msg);
+                    }
+                    else if(carouselMsg.column.length < limit && i==(resultLength-1)){
+                        msg = 'Kania bingung, ada yang salah, maaf ya, coba lagi deh';
+                        this.event.reply(msg);
+                    }
+                });
 
-                let myFunction = function(i){
-                    mapsClient.placesPhoto(photoQuery, (err,res)=>{
-                        if(err)
-                            console.log("Error query place photo : ", err);
-                        
-                        carouselMsg.addColumn(
-                            "https://" + res.req.socket._host + "" + res.req.path,
-                            trimString40(result[i].name),
-                            trimString60(result[i].vicinity),
-                            [
-                                ActionBuilder.createUriAction("Liat Map","https://www.google.com/maps/@"+result[i].geometry.location.lat+","+result[i].geometry.location.lng+",20z")
-                            ]
-                        );
-                        
-                        if(carouselMsg.column.length == limit){
-                            msg = carouselMsg.build();
-                            this.event.reply(msg);     
-                        }
-                        else if(carouselMsg.column.length == 0 && i == (resultLength-1)){
-                            console.log("Ngga ketemu apa apa");                      
-                            msg = 'Aku ngga bisa nemuin tempat makan dengan radius 1KM dari tempat kamu nih, coba jalan aja dulu';
-                            // this.event.reply(msg);
-                        }
-                        else if(carouselMsg.column.length < limit && i==(resultLength-1)){
-                            msg = 'Kania bingung, ada yang salah, maaf ya, coba lagi deh';
-                            // this.event.reply(msg);
-                        }
-                    });
-                }
+                // let myFunction = function(i){
+                    
+                // }
 
-                myFunction(i);
+                // myFunction(i);
             }
 
 
