8218cba4486fe7e7fd801a4bbaae726db250dc8c
ardikabs <ardikabs@gmail.com>
2017-11-17 17:44:46 +0700

    added

--- src/controllers/LINE/Messages.ts
+++ src/controllers/LINE/Messages.ts
@@ -243,13 +243,11 @@ class Messages{
                                         );
                                         
                                         if(carouselMsg.column.length === limit){
-                                            console.log("data masuk === limit"+limit);
                                             msg = carouselMsg.build();
                                             this.event.reply(msg);     
                                         }
-                                        else if(carouselMsg.column.length < limit && i === resultLength-1){
-                                            console.log("data masuk kurang dari limit"+carouselMsg.column.length);
-                                            
+                                        else if(carouselMsg.column.length < limit && i === resultLength){
+                                            console.log("Tempat titik");                                           
                                             // Kondisi ketika tempat tersedia namun tidak semuanya memiliki informasi foto
                                             msg = carouselMsg.build();                            
                                             this.event.reply(msg);
---
+++ src/controllers/Templates/ImageMapTemplates.ts
@@ -0,0 +1,72 @@
+
+
+// "type": "imagemap",
+// "baseUrl": "https://sibpens.gravicodev.id/kania/menu",
+// "altText": "Menunya Kania !!!",
+// "baseSize": {
+//   "height": 520,
+//   "width": 1040
+// },
+// "actions": [
+//   {
+//     "type": "message",
+//     "text": "subscribe",
+//     "area": {
+//       "x": 0,
+//       "y": 0,
+//       "width": 346,
+//       "height": 260
+//     }
+//   },
+//   {
+//     "type": "message",
+//     "text": "promo",
+//     "area": {
+//       "x": 346,
+//       "y": 0,
+//       "width": 348,
+//       "height": 260
+//     }
+//   },
+//   {
+//     "type": "message",
+//     "text": "voucher",
+//     "area": {
+//       "x": 695,
+//       "y": 0,
+//       "width": 346,
+//       "height": 260
+//     }
+//   },
+//   {
+//     "type":"message",
+//     "text":"premium",
+//     "area": {
+//       "x" : 0,
+//       "y" : 260,
+//       "width": 346,
+//       "height": 260
+//     }
+//   },
+//   {
+//     "type": "message",
+//     "text": "help",
+//     "area": {
+//       "x" : 346,
+//       "y" : 260,
+//       "width": 348,
+//       "height": 260
+//     }
+//   },
+//   {
+//     "type": "message",
+//     "text": "about",
+//     "area": {
+//       "x" : 695,
+//       "y" : 260,
+//       "width": 346,
+//       "height": 260
+//     }
+//   }
+// ]
+// }
